a0ec08692c22e690a4c1ec73a9da096d3d7b209e
Alexander Putilin <alexander.putilin@yahoo.de>
2026-06-10 11:48:55 +0200

    feat: Refactor account activation logic and add ActivateAccountView for user account activation

--- app_auth/api/views.py
+++ app_auth/api/views.py
@@ -1,10 +1,13 @@
+from django.utils.encoding import force_str
+from django.utils.http import urlsafe_base64_decode
+from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .serializers import RegisterSerializer
-from app_auth.services.token_service import generate_activation_token
+from app_auth.services.token_service import account_activation_token
 from app_auth.services.mail_service import Mailservice
 
 
@@ -15,7 +18,30 @@ class RegisterView(APIView):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            token = generate_activation_token(user)
+            token = account_activation_token.make_token(user)
             Mailservice.send_activation_email(user, token)
             return Response(status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class ActivateAccountView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request, uidb64, token):
+        user = get_user_from_uid(uidb64)
+        if not user or not account_activation_token.check_token(user, token):
+            return Response({"detail": "Activation failed"}, status=status.HTTP_400_BAD_REQUEST)
+        user.is_active = True
+        user.save(update_fields=["is_active"])
+        return Response({"message": "Account successfully activated"}, status=status.HTTP_200_OK)
+
+
+
+
+
+def get_user_from_uid(uidb64):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        return User.objects.get(pk=uid)
+    except Exception:
+        return None
