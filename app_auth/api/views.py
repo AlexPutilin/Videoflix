4aada4598d892a4c9766622b2416085a80a3f359
Alexander Putilin <alexander.putilin@yahoo.de>
2026-06-10 11:16:08 +0200

    feat: Add RegisterView for user registration with activation email

---
+++ app_auth/api/views.py
@@ -0,0 +1,21 @@
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .serializers import RegisterSerializer
+from app_auth.services.token_service import generate_activation_token
+from app_auth.services.mail_service import Mailservice
+
+
+class ReigsterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token = generate_activation_token(user)
+            Mailservice.send_activation_email(user, token)
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
