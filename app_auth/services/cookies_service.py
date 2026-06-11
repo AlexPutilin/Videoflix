956255401bf4df596556076d524e14b139074fdc
Alexander Putilin <alexander.putilin@yahoo.de>
2026-06-11 09:15:21 +0200

    feat: Add cookie management functions for setting and deleting token cookies

---
+++ app_auth/services/cookies_service.py
@@ -0,0 +1,20 @@
+def set_token_cookies(response, token_data):
+    response.set_cookie(
+        key="access_token",
+        value=token_data["access"],
+        httponly=True,
+        secure=False,
+        samesite="Lax",
+    )
+    response.set_cookie(
+        key="refresh_token",
+        value=token_data["refresh"],
+        httponly=True,
+        secure=False,
+        samesite="Lax",
+    )
+
+
+def delete_token_cookies(response):
+    response.delete_cookie('access_token')
+    response.delete_cookie('refresh_token')
