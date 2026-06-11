f992c9e82bd967d3201442bb04b96c4729138137
Alexander Putilin <alexander.putilin@yahoo.de>
2026-06-11 09:24:27 +0200

    feat: Add set_access_cookie function for setting access token cookies

--- app_auth/services/cookies_service.py
+++ app_auth/services/cookies_service.py
@@ -15,6 +15,16 @@ def set_token_cookies(response, token_data):
     )
 
 
+def set_access_cookie(response, access_token):
+    response.set_cookie(
+        key="access_token",
+        value=access_token,
+        httponly=True,
+        secure=False,
+        samesite="Lax",
+    )
+
+
 def delete_token_cookies(response):
     response.delete_cookie('access_token')
     response.delete_cookie('refresh_token')
