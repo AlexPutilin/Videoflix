5e7048157b75c61fe8ff23c7ce4a501d21d20c36
Alexander Putilin <alexander.putilin@yahoo.de>
2026-06-25 12:12:51 +0200

    feat: Implement email templates for account activation and password reset with inline logo

--- app_auth/services/mail_service.py
+++ app_auth/services/mail_service.py
@@ -1,9 +1,11 @@
 from django.conf import settings
-from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
 from django.contrib.auth.tokens import default_token_generator
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
+from email.message import MIMEPart
+from pathlib import Path
 
 
 def send_activation_email(user, token):
@@ -37,15 +39,34 @@ def send_password_reset_email(user):
 
 
 def send_templated_email(template_name, subject, recipient, context):
-    text_path = f"app_auth/emails/{template_name}.txt"
-    html_path = f"app_auth/emails/{template_name}.html"
+    text_path = f"emails/{template_name}.txt"
+    html_path = f"emails/{template_name}.html"
+    context["logo_cid"] = "videoflix_logo"
     message = render_to_string(text_path, context)
     html_message = render_to_string(html_path, context)
-    send_mail(
+    email = EmailMultiAlternatives(
         subject=subject,
-        message=message,
+        body=message,
         from_email=settings.DEFAULT_FROM_EMAIL,
-        recipient_list=[recipient],
-        html_message=html_message,
-        fail_silently=False,
+        to=[recipient]
     )
+    email.attach_alternative(html_message, "text/html")
+    attach_inline_image(email)
+    email.send(fail_silently=False)
+
+
+def attach_inline_image(email):
+    image_path = (Path(settings.BASE_DIR) / "app_auth/static/images/logo.png")
+    if not image_path.exists():
+        return
+    with open(image_path, "rb") as image_file:
+        image_data = image_file.read()
+    inline_image = MIMEPart()
+    inline_image.set_content(
+        image_data,
+        maintype="image",
+        subtype="png",
+        disposition="inline",
+        cid="<videoflix_logo>",
+    )
+    email.attach(inline_image)
