99d75a53bc7526a5b3339d7b879bd78fbafa31f8
Alexander Putilin <alexander.putilin@yahoo.de>
2026-07-02 10:07:04 +0200

    fix: Remove leading slash from password reset URL path

--- app_auth/api/urls.py
+++ app_auth/api/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("token/refresh/", RefreshTokenView.as_view(), name="token_refresh"),
-    path("/password_reset/", PasswordResetView.as_view(), name="password_reset"),
+    path("password_reset/", PasswordResetView.as_view(), name="password_reset"),
     path("password_confirm/<uidb64>/<token>/", PasswordConfirmView.as_view(), name="password_confirm"),
 ]
